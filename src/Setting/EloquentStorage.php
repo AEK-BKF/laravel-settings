a5b1f51d1dac68d7e95d44e4b20208bf8dbc33cc
AEK-BKF <akaderb@gmail.com>
2017-05-03 17:01:55 +0200

    Update EloquentStorage.php

--- src/Setting/EloquentStorage.php
+++ src/Setting/EloquentStorage.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class EloquentStorage extends Eloquent implements SettingStorageInterface
 {
-    protected $fillable = ['key', 'value', 'locale'];
+    protected $fillable = ['key', 'value', 'locale', 'user_id', 'is_global'];
 
     protected $table = 'settings';
 
     public $timestamps = false;
 
-    public static function retrieve($key, $lang = null)
+    public static function retrieve($key, $lang = null, $user_id = null)
     {
         $setting = static::where('key', $key);
 
@@ -21,22 +21,27 @@ class EloquentStorage extends Eloquent implements SettingStorageInterface
         } else {
             $setting = $setting->whereNull('locale');
         }
+        if (!is_null($user_id)) {
+            $setting = $setting->where('user_id', $user_id);
+        }
 
         return $setting->first();
     }
 
-    public static function store($key, $value, $lang)
+    public static function store($key, $value, $lang, $user_id)
     {
         $setting = ['key' => $key, 'value' => $value];
 
         if (!is_null($lang)) {
             $setting['locale'] = $lang;
         }
-        
+        if (!is_null($user_id)) {
+            $setting['user_id'] = $user_id;
+        }
         static::create($setting);
     }
 
-    public static function modify($key, $value, $lang)
+    public static function modify($key, $value, $lang, $user_id)
     {
         if (!is_null($lang)) {
             $setting = static::where('locale', $lang);
@@ -44,12 +49,12 @@ class EloquentStorage extends Eloquent implements SettingStorageInterface
             $setting = new static();
         }
 
-        $setting->where('key', $key)->update(['value' => $value]);
+        $setting->where('key', $key)->where('user_id', $user_id)->update(['value' => $value]);
     }
 
-    public static function forget($key, $lang)
+    public static function forget($key, $lang, $user_id)
     {
-        $setting = static::where('key', $key);
+        $setting = static::where('key', $key)->where('user_id', $user_id);
 
         if (!is_null($lang)) {
             $setting = $setting->where('locale', $lang);
